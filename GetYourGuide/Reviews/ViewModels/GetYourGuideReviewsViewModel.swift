0d8b2a63882726827022bed3e834c64fc793f4ed
Leonardo Kaminski Ferreira <leonardo@mindvalley.com>
2019-07-30 17:22:08 -0300

    Added reviews view model

---
+++ GetYourGuide/Reviews/ViewModels/GetYourGuideReviewsViewModel.swift
@@ -0,0 +1,52 @@
+//
+//  GetYourGuideReviewsViewModel.swift
+//  GetYourGuide
+//
+//  Created by Leonardo Vinicius Kaminski Ferreira on 30/07/19.
+//  Copyright © 2019 Leonardo Ferreira. All rights reserved.
+//
+
+import Foundation
+
+enum GetYourGuideReviewTypeCell: String {
+    case review = "GetYourGuideReview"
+}
+
+struct GetYourGuideReviewCell {
+    var type: GetYourGuideReviewTypeCell = .review
+    var review: GetYourGuideReviewModel?
+    
+    init(_ review: GetYourGuideReviewModel?) {
+        self.review = review
+    }
+}
+
+class GetYourGuideReviewsViewModel {
+    
+    let requester: NetworkingRequester = NetworkingRequester()
+    var reviewsRequest: GetYourGuideReviewsRequest = GetYourGuideReviewsRequest()
+    
+    var isLoadingContent: Bool = false
+    
+    func getData(response: @escaping ([GetYourGuideReviewCell]) -> Void) {
+        isLoadingContent = true
+        response([])
+        
+        requester.performRequest(reviewsRequest) { [weak self] (data: GetYourGuideReviewsModel?, error) in
+            self?.isLoadingContent = false
+            
+            if let error = error {
+                print(error)
+                return
+            }
+            
+            var cells: [GetYourGuideReviewCell] = []
+            for review in data?.reviews ?? [] {
+                cells.append(GetYourGuideReviewCell.init(review))
+            }
+            
+            response(cells)
+        }
+    }
+    
+}
