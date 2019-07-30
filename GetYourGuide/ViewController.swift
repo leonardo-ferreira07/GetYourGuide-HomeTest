08da91d67203e0caa17b7ffdfc0a71d6aa248e62
Leonardo Kaminski Ferreira <leonardo@mindvalley.com>
2019-07-30 14:18:41 -0300

    Added initial project configs

---
+++ GetYourGuide/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GetYourGuide
+//
+//  Created by Leonardo Vinicius Kaminski Ferreira on 30/07/19.
+//  Copyright © 2019 Leonardo Ferreira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
